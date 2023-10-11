79a17dfca7fc28a040c36b4121f3e5d0c7dd569e
PabloMurriello <pablo79873@gmail.com>
2023-10-11 20:48:07 -0300

    Update ReservaData.java

--- src/pfgranhotel/accesoDatos/ReservaData.java
+++ src/pfgranhotel/accesoDatos/ReservaData.java
@@ -16,14 +16,15 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
+import org.mariadb.jdbc.Statement;
 import pfgranhotel.entidades.Huesped;
 import pfgranhotel.entidades.Reserva;
 
 public class ReservaData {
 
     private Connection con = null;
-    private HuespedData hData=new HuespedData();
-    private HabitacionData habData=new HabitacionData();
+    private HuespedData hData = new HuespedData();
+    private HabitacionData habData = new HabitacionData();
 
     public ReservaData() {
         con = Conexion.getConexion();
@@ -42,7 +43,7 @@ public class ReservaData {
 
         /// Marce, verificà que no me haya mandado alguna cagada
         try {
-            PreparedStatement ps = con.prepareStatement(sql);
+            PreparedStatement ps = con.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
             ps.setInt(1, reserva.getHuesped().getIdHuesped());
             ps.setInt(2, reserva.getHabitacion().getIdHabitacion());
             ps.setDate(3, Date.valueOf(reserva.getFechaIn()));
@@ -52,7 +53,8 @@ public class ReservaData {
             ps.setBoolean(7, reserva.isEstado());
 
             ps.executeUpdate();
-            ResultSet rs = ps.executeQuery();
+            ResultSet rs = ps.getGeneratedKeys();
+            
             if (rs.next()) {
                 reserva.setIdReserva(rs.getInt(1));
 
@@ -66,29 +68,65 @@ public class ReservaData {
         }
 
     }
-    
-    public List<Reserva> reservar(){
-    List<Reserva> reserva =new ArrayList<>();
-    String sql="SELECT `idReserva`, `DNI`, `idHabitacion` FROM `reserva`";
-    
-      try {
-            PreparedStatement ps= con.prepareStatement(sql);
-          ResultSet rs=ps.executeQuery();
-            while (rs.next()) {  
-                Reserva res= new Reserva();
-                System.out.println("idReserva "+rs.getInt("idReserva") );
-                System.out.println("DNI "+rs.getInt("DNI"));
-                System.out.println("idHabitacion "+rs.getInt("idHabitacion"));
-                }
+
+    public List<Reserva> reservar() {
+        List<Reserva> reserva = new ArrayList<>();
+        String sql = "SELECT `idReserva`, `DNI`, `idHabitacion` FROM `reserva`";
+
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Reserva res = new Reserva();
+                System.out.println("idReserva " + rs.getInt("idReserva"));
+                System.out.println("DNI " + rs.getInt("DNI"));
+                System.out.println("idHabitacion " + rs.getInt("idHabitacion"));
+            }
             ps.close();
-              } catch (SQLException ex) {
+        } catch (SQLException ex) {
             Logger.getLogger(ReservaData.class.getName()).log(Level.SEVERE, null, ex);
         }
+
+        return reserva;
+    }
+    //dAR DE BAJA RESERVA
+
+    public void bajaReserva(Reserva reserva) throws SQLException {
+
+        String sql = " UPDATE `reserva` SET `estado`= 0 WHERE 'DNI'=? ";
+
+        PreparedStatement ps = con.prepareStatement(sql);
+        int fila = ps.executeUpdate();
+
+        if (fila > 0) {
+            JOptionPane.showMessageDialog(null, "Reserva dada de baja exitosamente");
+        } else {
+            JOptionPane.showMessageDialog(null, "Reserva no dada de baja");
+
+        }
+    }
+
+    //HUESPED POR HABITACIÒN 
+    public void huespedxHabitacion (Reserva reserva)throws SQLException {
+      String sql= "SELECT DNI, Apellido FROM 'huesped' JOIN 'reserva' ON huesped.idHuesped=reserva.idHuesped AND habitacion.idHabitacion =?";  
+      
+PreparedStatement ps = con.prepareStatement(sql);
+        ResultSet resultado= ps.executeQuery();
+            
+        while(resultado.next()) {
+            
+            System.out.println("DNI:" + resultado.getInt("DNI"));
+            System.out.println("Apellido:" + resultado.getString("Apellido"));
+        }      
+}
     
+    // habitaciònxHuesped
     
-    return reserva;
- }
-                
-            }
+    public void habitacionxHuesped(Reserva reserva) throws SQLException{
+        String sql = "SELECT idHabitacion FROM 'habitacion' JOIN 'reserva' ON habitacion.idHabitacion= reserva.idHabitacion AND reserva.DNI=?";
+           
+        
+    }
+}
 
 //mmm
