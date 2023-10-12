0db88561e760ba6e54f67093ab2ea682f7853ac3
Davidstafe <141676686+Davidstafe@users.noreply.github.com>
2023-10-12 18:16:03 -0300

    tipo habitacion

--- src/pfgranhotel/accesoDatos/TipoDeHabitacionData.java
+++ src/pfgranhotel/accesoDatos/TipoDeHabitacionData.java
@@ -30,7 +30,10 @@ public class TipoDeHabitacionData {
         
        ////****quiero hacer un metodo del tipo de habitacion Simple para listarlo
       /// en el menu
-        /*public  Arraylist<TipoDeHabitacion> listarTipoSimple(){
+    }
+    
+    ///llamo a la lista de tipo de hab. Simple para desp mostrar en la tabla
+    public ArrayList<TipoDeHabitacion> listarTipoSimple(){
             ArrayList<TipoDeHabitacion> tipoHab = new ArrayList<>();
             String sql="SELECT * FROM habitacion WHERE tipo_habitacion IN ('Simple')";
             
@@ -48,15 +51,111 @@ public class TipoDeHabitacionData {
                  thab.setTipoDeHabitacion(rs.getString("tipoDeHabitacion"));
                  thab.setPrecio(rs.getInt("precio"));
                  thab.setEstado(rs.getBoolean("estado"));
-                 TipoDeHabitacion.add(thab);  ///xq no me lo agrega????    :(
+                 tipoHab.add(thab);  ///xq no me lo agrega????    :(
+                 
+                
              }
      
         } catch (SQLException ex) {
             JOptionPane.showMessageDialog(null,"Error al listar las habitaciones simples"+ ex);
         }
+        return tipoHab;
            
         
-        }  */
+        }  
+    
+    public ArrayList<TipoDeHabitacion> listarTipoDoble(){
+         ArrayList<TipoDeHabitacion> tipoDobl = new ArrayList<>();
+            String sql="SELECT * FROM habitacion WHERE tipo_habitacion IN ('Dobles')";
+            
+            PreparedStatement ps;
+        try {
+            ps = con.prepareStatement(sql);
+             ResultSet rs=ps.executeQuery();
+             
+             while(rs.next()){
+                 TipoDeHabitacion thab = new TipoDeHabitacion();
+                 thab.setIdTipo(rs.getInt("idTipo"));
+                 thab.setCantPers(rs.getInt("cantPers"));
+                 thab.setCantCamas(rs.getInt("cantCamas"));
+                 thab.setTipoDeCamas(rs.getString("tipoDeCamas"));
+                 thab.setTipoDeHabitacion(rs.getString("tipoDeHabitacion"));
+                 thab.setPrecio(rs.getInt("precio"));
+                 thab.setEstado(rs.getBoolean("estado"));
+                 tipoDobl.add(thab); 
+                 
+                
+             }
+     
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null,"Error al listar las habitaciones dobles"+ ex);
+        }
+        return tipoDobl;
+           
+        
+    }
+    
+    public ArrayList<TipoDeHabitacion> listarTipoTriple(){
+        
+         ArrayList<TipoDeHabitacion> tipoTri = new ArrayList<>();
+            String sql="SELECT * FROM habitacion WHERE tipo_habitacion IN ('Triple')";
+            
+            PreparedStatement ps;
+        try {
+            ps = con.prepareStatement(sql);
+             ResultSet rs=ps.executeQuery();
+             
+             while(rs.next()){
+                 TipoDeHabitacion thab = new TipoDeHabitacion();
+                 thab.setIdTipo(rs.getInt("idTipo"));
+                 thab.setCantPers(rs.getInt("cantPers"));
+                 thab.setCantCamas(rs.getInt("cantCamas"));
+                 thab.setTipoDeCamas(rs.getString("tipoDeCamas"));
+                 thab.setTipoDeHabitacion(rs.getString("tipoDeHabitacion"));
+                 thab.setPrecio(rs.getInt("precio"));
+                 thab.setEstado(rs.getBoolean("estado"));
+                 tipoTri.add(thab);  
+                 
+                
+             }
+     
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null,"Error al listar las habitaciones triples"+ ex);
+        }
+        return tipoTri;
+           
+    }
+    
+    public ArrayList<TipoDeHabitacion> listarTipoSuite(){
+        
+         ArrayList<TipoDeHabitacion> tipoSuite = new ArrayList<>();
+            String sql="SELECT * FROM habitacion WHERE tipo_habitacion IN ('Suite')";
+            
+            PreparedStatement ps;
+        try {
+            ps = con.prepareStatement(sql);
+             ResultSet rs=ps.executeQuery();
+             
+             while(rs.next()){
+                 TipoDeHabitacion thab = new TipoDeHabitacion();
+                 thab.setIdTipo(rs.getInt("idTipo"));
+                 thab.setCantPers(rs.getInt("cantPers"));
+                 thab.setCantCamas(rs.getInt("cantCamas"));
+                 thab.setTipoDeCamas(rs.getString("tipoDeCamas"));
+                 thab.setTipoDeHabitacion(rs.getString("tipoDeHabitacion"));
+                 thab.setPrecio(rs.getInt("precio"));
+                 thab.setEstado(rs.getBoolean("estado"));
+                 tipoSuite.add(thab);
+                 
+                
+             }
+     
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null,"Error al listar las habitaciones simples"+ ex);
+        }
+        return tipoSuite;
+           
+    }
 }
-}    
+    
 
