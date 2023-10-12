3c6e95c22e2bcb510a0a6036d2df284987df5209
Davidstafe <David.fmdrm@gmail.com>
2023-10-11 21:43:39 -0300

    Creo q es por aca

--- src/pfgranhotel/accesoDatos/TipoDeHabitacionData.java
+++ src/pfgranhotel/accesoDatos/TipoDeHabitacionData.java
@@ -6,6 +6,14 @@
 package pfgranhotel.accesoDatos;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import pfgranhotel.entidades.TipoDeHabitacion;
 
 /**
  *
@@ -20,10 +28,35 @@ public class TipoDeHabitacionData {
     
         con=Conexion.getConexion();
         
-       /// public void TipoDeHabitacionData(){
+       ////****quiero hacer un metodo del tipo de habitacion Simple para listarlo
+      /// en el menu
+        /*public  Arraylist<TipoDeHabitacion> listarTipoSimple(){
+            ArrayList<TipoDeHabitacion> tipoHab = new ArrayList<>();
+            String sql="SELECT * FROM habitacion WHERE tipo_habitacion IN ('Simple')";
             
-            
-        }    
+            PreparedStatement ps;
+        try {
+            ps = con.prepareStatement(sql);
+             ResultSet rs=ps.executeQuery();
+             
+             while(rs.next()){
+                 TipoDeHabitacion thab = new TipoDeHabitacion();
+                 thab.setIdTipo(rs.getInt("idTipo"));
+                 thab.setCantPers(rs.getInt("cantPers"));
+                 thab.setCantCamas(rs.getInt("cantCamas"));
+                 thab.setTipoDeCamas(rs.getString("tipoDeCamas"));
+                 thab.setTipoDeHabitacion(rs.getString("tipoDeHabitacion"));
+                 thab.setPrecio(rs.getInt("precio"));
+                 thab.setEstado(rs.getBoolean("estado"));
+                 TipoDeHabitacion.add(thab);  ///xq no me lo agrega????    :(
+             }
+     
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null,"Error al listar las habitaciones simples"+ ex);
+        }
+           
+        
+        }  */
 }
-    
+}    
 
